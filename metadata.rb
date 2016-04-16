1154610385c606268d0a1a9a3151049eeb56e6ed
Georgi Lambov <georgi.lambov@gmail.com>
2016-04-16 15:40:43 +0300

    ps_watcher cookbook template.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'ps_watcher'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures ps_watcher'
+long_description 'Installs/Configures ps_watcher'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: ps_watcher
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
